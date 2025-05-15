0fe9c5a7075792a45b92be25204067d55a6eaee2
Yudann <yudannnnn80@gmail.com>
2025-05-15 19:55:29 +0700

    feat: add logout button

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import LogoutButton from "./LogoutButton";
 
 const menuItems = [
   { name: "Dashboard", path: "/dashboard" },
@@ -24,23 +25,26 @@ export default function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <aside className="w-[350px] bg-white shadow-md h-screen p-8">
-      <h1 className="text-xl font-bold mb-6">EasyAccount</h1>
-      <nav className="flex flex-col gap-2">
-        {menuItems.map((item) => (
-          <Link
-            key={item.path}
-            href={item.path}
-            className={clsx(
-              "px-3 py-2 rounded hover:bg-gray-200",
-              pathname === item.path &&
-                "bg-blue-100 text-blue-600 font-semibold"
-            )}
-          >
-            {item.name}
-          </Link>
-        ))}
-      </nav>
+    <aside className="w-[350px] bg-white shadow-md h-screen p-8 flex flex-col  justify-between">
+      <div>
+        <h1 className="text-xl font-bold mb-6">EasyAccount</h1>
+        <nav className="flex flex-col gap-2">
+          {menuItems.map((item) => (
+            <Link
+              key={item.path}
+              href={item.path}
+              className={clsx(
+                "px-3 py-2 rounded hover:bg-gray-200",
+                pathname === item.path &&
+                  "bg-blue-100 text-blue-600 font-semibold"
+              )}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
+      <LogoutButton />
     </aside>
   );
 }
