168dee8b2111f28a8686029541da597f842a2504
Yudann <yudannnnn80@gmail.com>
2025-05-21 18:42:38 +0700

    feat: add new account

--- src/lib/accounts.ts
+++ src/lib/accounts.ts
@@ -1,19 +1,20 @@
-// lib/accounts.ts
-export const akunList = [
- { kode: '101', nama: 'Kas', tipe: 'debit' },
-  { kode: '102', nama: 'Piutang Usaha', tipe: 'debit'  },
-  { kode: '103', nama: 'Perlengkapan', tipe: 'debit' },
-  { kode: '104', nama: 'Sewa Dibayar di Muka', tipe: 'debit'  },
-  { kode: '105', nama: 'Peralatan', tipe: 'debit'  },
-  { kode: '201', nama: 'Hutang Usaha', tipe: 'debit' },
-  { kode: '202', nama: 'Hutang Bank', tipe: 'debit'  },
-  { kode: '301', nama: 'Modal Pemilik', tipe: 'debit'  },
-  { kode: '302', nama: 'Prive Pemilik', tipe: 'debit'  },
-  { kode: '401', nama: 'Pendapatan Jasa', tipe: 'debit'  },
-  { kode: '402', nama: 'Pendapatan Bunga', tipe: 'debit'  },
-  { kode: '501', nama: 'Beban Listrik', tipe: 'debit'  },
-  { kode: '502', nama: 'Beban Pemeliharaan', tipe: 'debit'  },
-  { kode: '503', nama: 'Beban Telepon', tipe: 'debit'  },
-  { kode: '504', nama: 'Beban Lain-Lain', tipe: 'debit'  },
-  { kode: '505', nama: 'Beban Gaji', tipe: 'debit'  },
+import { Akun } from "@/store/slices/accountSlice";
+
+export const akunList: Akun[]  = [
+  { kode: '101', nama: 'Kas', tipe: 'Aset' },
+  { kode: '102', nama: 'Piutang Usaha', tipe: 'Aset' },
+  { kode: '103', nama: 'Perlengkapan', tipe: 'Aset' },
+  { kode: '104', nama: 'Sewa Dibayar di Muka', tipe: 'Aset' },
+  { kode: '105', nama: 'Peralatan', tipe: 'Aset' },
+  { kode: '201', nama: 'Hutang Usaha', tipe: 'Liabilitas' },
+  { kode: '202', nama: 'Hutang Bank', tipe: 'Liabilitas' },
+  { kode: '301', nama: 'Modal Pemilik', tipe: 'Ekuitas' },
+  { kode: '302', nama: 'Prive Pemilik', tipe: 'Ekuitas' },
+  { kode: '401', nama: 'Pendapatan Jasa', tipe: 'Pendapatan' },
+  { kode: '402', nama: 'Pendapatan Bunga', tipe: 'Pendapatan' },
+  { kode: '501', nama: 'Beban Listrik', tipe: 'Beban' },
+  { kode: '502', nama: 'Beban Pemeliharaan', tipe: 'Beban' },
+  { kode: '503', nama: 'Beban Telepon', tipe: 'Beban' },
+  { kode: '504', nama: 'Beban Lain-Lain', tipe: 'Beban' },
+  { kode: '505', nama: 'Beban Gaji', tipe: 'Beban' },
 ];
